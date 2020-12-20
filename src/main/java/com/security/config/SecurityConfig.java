94fb7fead691873d08a18cf619a89721ea47a0e0
Jun Yu <zlsl7343@gmail.com>
2020-12-20 21:19:11 +0900

    SecurityConfig 정적 파일 관련 Config 내용 추가

--- src/main/java/com/security/config/SecurityConfig.java
+++ src/main/java/com/security/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.security.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,15 +12,33 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    
+    /*
+        정적파일(js, template, css 등) Authentication 무시
+     */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    /*
+        기본 Security 설정
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers("/", "/hello").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().authenticated();
+
+        http.formLogin()
                 .and()
-                .formLogin()
+                .rememberMe()
                 .and()
                 .httpBasic();
+
+        http.logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/");
     }
 
     @Bean
