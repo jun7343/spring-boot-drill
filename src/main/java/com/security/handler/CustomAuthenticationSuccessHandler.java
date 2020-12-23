a4b122f4b1d9079467e2e526d30736afe2a2317b
Jun Yu <zlsl7343@gmail.com>
2020-12-23 22:36:09 +0900

    CustomAuthenticationSuccessHandler class 추가

---
+++ src/main/java/com/security/handler/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,39 @@
+package com.security.handler;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    // 사용자가 인증 처리가 필요한 URL Session 정보가 담긴 RequestCache
+    private RequestCache requestCache = new HttpSessionRequestCache();
+
+    // 인증 성공 후 이전 요청한 URL 로 Redirect
+    private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        SavedRequest savedRequest = requestCache.getRequest(request, response);
+
+        // 기본 Page
+        setDefaultTargetUrl("/");
+
+        // savedRequest 는 null 일 수 있다. 바로 Login Page 로 이동시 null
+        if (savedRequest != null) {
+            String redirectUrl = savedRequest.getRedirectUrl();
+            redirectStrategy.sendRedirect(request, response, redirectUrl);
+        } else {
+            redirectStrategy.sendRedirect(request, response, getDefaultTargetUrl());
+        }
+    }
+}
